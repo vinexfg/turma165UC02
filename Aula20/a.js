47ea49c52139e97b5c67fc62200bb02a2b127e70
vinexfg <vinysilvaa20@gmail.com>
2025-10-08 21:04:37 -0300

    a

--- Aula20/a.js
+++ Aula20/a.js
@@ -1,38 +1,30 @@
-let texto = document.querySelector('h1')
-texto.textContent = "Aula manipulacao do Dom"
+let texto = document.querySelector("h1");
+texto.textContent = "Aula manipulacao do Dom";
 
-texto.innerText += "<u> test </u>" 
+texto.innerText += "<u> test </u>";
 
-
-document.body.innerHTML += "<main> <h3> Criando elementos inline </h3> </main>"
+document.body.innerHTML += "<main> <h3> Criando elementos inline </h3> </main>";
 
 // craindo um paragrafo no HTML
 let novoParagrafro = document.createElement("p");
-novoParagrafro.textContent = 'Texto craido dinaminicamente'
-document.body.appendChild(novoParagrafro)
+novoParagrafro.textContent = "Texto craido dinaminicamente";
+document.body.appendChild(novoParagrafro);
 
-
-function adicionarItem(){
-    let item = document.getElementById("item").value;
-    let novoItem = document.createElement("li")
-    novoItem.textContent = item;
-    document.getElementById('lista').appendChild(novoItem);
-    document.getElementById('item').value = "";
-}   
-
-
-let idade = parseInt(prompt('Digite sua idade:'));
-
-if(idade < 18){
-    alert("voce e menor idade")
+function adicionarItem() {
+  let item = document.getElementById("item").value;
+  let novoItem = document.createElement("li");
+  novoItem.textContent = item;
+  document.getElementById("lista").appendChild(novoItem);
+  document.getElementById("item").value = "";
 }
+a;
 
-else if( idade < 60){
-    alert(
-        'voce e aduto'
-    )
-}
+let idade = parseInt(prompt("Digite sua idade:"));
 
-else{
-    alert(' voce e idoso ')
+if (idade < 18) {
+  alert("voce e menor idade");
+} else if (idade < 60) {
+  alert("voce e aduto");
+} else {
+  alert(" voce e idoso ");
 }
