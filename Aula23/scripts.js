ce618b88ce542f0e94eb5f42e61bbc83a291f7d1
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-10-13 20:46:23 -0300

    a

---
+++ Aula23/scripts.js
@@ -0,0 +1,52 @@
+
+
+
+// const numero = parseInt(prompt('Digite um numero de 0 a 4:'))
+
+//     switch (numero){
+//     case 0:
+//         console.log('Aula de JS Hoje');
+//         break;
+//     case 1:
+//         console.log('Aula de JS Amanha')
+//         break;
+//     case 2:
+//         console.log(' Aula de JS Sempre')
+//         break;
+//     default:
+//         console.log(' opcao invalida. ')
+
+    // }
+
+
+const dia = parseInt(prompt("Digite de 1 a  8"))
+
+switch (dia) {
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+    
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+    
+    case 1:
+         console.log(' Segunda-feira')
+        break;
+
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+    case 1:
+        console.log(' Segunda-feira')
+        break;
+
+    default:
+        console.log('Error')
+} 
