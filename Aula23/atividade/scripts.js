590c18de6bbc7cc26388e08af3b791d763aa88b4
vinexfg <vinysilvaa20@gmail.com>
2025-10-14 17:16:34 -0300

    a

--- Aula23/atividade/scripts.js
+++ Aula23/atividade/scripts.js
@@ -1,34 +1,29 @@
-// function valores(){
-//     let X = document.getElementById('valor1').value
-//     let Y = document.getElementById('valor2').value
-//     let Z = document.getElementById('valor3').value
-// }
+function calculo(tipo) {
+  let X = Number(document.getElementById("valor1").value);
+  let Y = Number(document.getElementById("valor2").value);
+  let Z = Number(document.getElementById("valor3").value);
+  let digito = document.getElementById("escrito");
 
-function calculo1(){
-    let X = document.getElementById('valor1').value
-    let Y = document.getElementById('valor2').value
-    let Z = document.getElementById('valor3').value
-    let digito = document.getElementById('escrito')
+  let resultado;
 
-    let resultado = (X + Y + Z)
-    digito.innerText(`Resultado: ${resultado}`)
+  switch (tipo) {
+    case "aritmetica":
+      resultado = (X + Y + Z) / 3;
+      digito.innerText = `Média Aritmética: ${resultado.toFixed(2)}`;
+      break;
+
+    case "ponderada":
+      resultado = (X * 3 + Y * 3 + Z * 4) / 10;
+      digito.innerText = `Média Ponderada: ${resultado.toFixed(2)}`;
+      break;
+
+    case "geometrica":
+      resultado = (X * Y * Z) ** (1 / 3);
+      digito.innerText = `Média Geométrica: ${resultado.toFixed(2)}`;
+      break;
+
+    default:
+      digito.innerText = "Opção inválida!";
+      break;
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-//     }    switch (X, Y, Z) {
-//         case value:
-            
-//             break;
-    
-//         default:
-//             break;
-// }
