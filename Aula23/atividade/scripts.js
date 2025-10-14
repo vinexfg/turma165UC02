6af17f48612a7eedb1cb3cd20602c97d5e5473c1
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-10-13 21:25:05 -0300

    a

--- Aula23/atividade/scripts.js
+++ Aula23/atividade/scripts.js
@@ -5,15 +5,16 @@
 // }
 
 function calculo1(){
-    let X = parseFloat(document.getElementById('valor1')).value
-    let Y = parseFloat(document.getElementById('valor2')).value
-    let Z = parseFloat(document.getElementById('valor3')).value
+    let X = document.getElementById('valor1').value
+    let Y = document.getElementById('valor2').value
+    let Z = document.getElementById('valor3').value
     let digito = document.getElementById('escrito')
 
     let resultado = (X + Y + Z)
-    alert(resultado)
-    digito.textContent(`Resultado: ${resultado}`)
+    digito.innerText(`Resultado: ${resultado}`)
 }
+
+
 
 
 
