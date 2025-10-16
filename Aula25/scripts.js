3edb0832426ea3dfb2460acf4a41b84150215227
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-10-15 21:23:41 -0300

    oi

--- Aula25/scripts.js
+++ Aula25/scripts.js
@@ -1,3 +1,20 @@
-function minilicao2(){
+// function mutiplicacao(){
     
-}
+    
+//     let valor = parseInt(document.getElementById('number').value)
+//     let div = document.getElementById('divin')
+//     div.innerText = ''
+//     if(isNaN(valor)){
+//         console.log(' DIgitie um valor')
+//     }else{
+//         for (let i = 0; i<11; i++){
+//         const texto = document.createElement('p')
+//         texto.innerText = `${valor} X ${i} = ${valor * i}`
+//         div.appendChild(texto)
+//     }
+//     }
+   
+// }
+
+
+let meuArray = [1, "dois", true, {chave: "valor"}]
