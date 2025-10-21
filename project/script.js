ee5f33a68445dcde879833f2a1f89f8ad01e40e8
vinexfg <vinysilvaa20@gmail.com>
2025-10-21 10:12:19 -0300

    a

--- project/script.js
+++ project/script.js
@@ -1,36 +1,32 @@
 let tarefas = [];
 
-let input = document.querySelector('#tarefa');
-let lista = document.querySelector('#lista');
-let btnAdd = document.querySelector('#btnAdd');
+let input = document.querySelector("#tarefa");
+let lista = document.querySelector("#lista");
+let btnAdd = document.querySelector("#btnAdd");
 
-btnAdd.addEventListener("click", () =>{
-    if(input.value === ""){
-        alert("Digite uma tarefa");
-    }else{
-        tarefas.push(input.value)
-    }
-    input.value = '';
+btnAdd.addEventListener("click", () => {
+  if (input.value === "") {
+    alert("Digite uma tarefa");
+  } else {
+    tarefas.push(input.value);
+  }
+  input.value = "";
+});
 
-})
+function mostrarTarefas() {
+  lista.textContent = "";
+  tarefas.forEach((t) => {
+    let li = document.createElement("li");
+    li.textContent = t;
+    let editar = document.createElement("button");
+    editar.textContent = "Editar";
 
-function mostrarTarefas(){
-    lista.textContent = ''
-    tarefas.forEach(t=>{
-        let li  = document.createElement('li');
-        li.textContent = t
-        let editar = document.createElement('button')
-        editar.textContent = 'Editar'
-
-        let excluir = document.createElement('button')
-        excluir.textContent = 'Excluir'
-        li.appendChild('editar')
-        li.appendChild('excluir')
-        lista.appendChild(li);
-    })
+    let excluir = document.createElement("button");
+    excluir.textContent = "Excluir";
+    li.appendChild("editar");
+    li.appendChild("excluir");
+    lista.appendChild(li);
+  });
 }
 
-
-
 /// o que tenho que estudar  (addEventListener) (forEach) (appendChild) (===) ()
-
