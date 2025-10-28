5946ef6ed621bb3cbf6bd88ce6e6bc6076459e24
vinexfg <vinysilvaa20@gmail.com>
2025-10-28 15:32:53 -0300

    a

--- Aula29/src/components/Header/Header.jsx
+++ Aula29/src/components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import  style from "./Header.module.css"
-export default function Header(){
-    return(
-        <>
-            <header>
-            <h1>
-                MEU SITE
-            </h1>
-            <nav>
-                <ul>
-                    <li><a href="#">INICIO</a></li>
-                    <li><a href="#">SOBRE</a></li>
-                    <li><a href="#">CONTATO</a></li>
-                </ul>
-            </nav>
-            </header>
-        </>
-    )
+import style from "./Header.module.css";
+export default function Header() {
+  return (
+    <>
+      <header>
+        <h1>MEU SITE</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">INICIO</a>
+            </li>
+            <li>
+              <a href="#">SOBRE</a>
+            </li>
+            <li>
+              <a href="#">CONTATO</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
