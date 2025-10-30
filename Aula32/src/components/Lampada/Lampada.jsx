f21035fdaaa162576b9270d0aac0a3b0e3312e34
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-10-29 21:22:34 -0300

    a

--- Aula32/src/components/Lampada/Lampada.jsx
+++ Aula32/src/components/Lampada/Lampada.jsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Lampada(){
+export default function Lampada() {
+  const [luz, setLuz] = useState(false);
 
-    const [ligado, setLigado] = useState(0);
-    return(
-        <>
+  return (
+    <div>
+      {
+        luz
+          ? <img src="ligada.jpg" alt="Lâmpada ligada" />
+          : <img src="desligada.jpg" alt="Lâmpada desligada" />
+      }
 
-            <div>
-                <button>Desligar</button>
-                {/* <button onClick={()=> setLigado(setLigado == true ? setLigado == false: )} */}
-                <img src="https://img.freepik.com/vetores-gratis/lampada-realistica-isolada-de-vetor-sobre-branco_1284-41931.jpg?semt=ais_hybrid&w=740&q=80" alt="Lampada acessa" />
-
-            </div>
-        </>
-    )
+      <button onClick={() => setLuz(luz ? false: true)}>
+        {luz ? "Apagar luz" : "Acender luz"}
+      </button>
+    </div>
+  );
 }
