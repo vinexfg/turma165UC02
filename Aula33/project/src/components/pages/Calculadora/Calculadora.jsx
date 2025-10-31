2de458c2eef4a46c1ee2b977a2f0fafb178c28a8
vinexfg <vinysilvaa20@gmail.com>
2025-10-31 16:32:55 -0300

    Update Calculadora.jsx

--- Aula33/project/src/components/pages/Calculadora/Calculadora.jsx
+++ Aula33/project/src/components/pages/Calculadora/Calculadora.jsx
@@ -1,64 +1,61 @@
-import style from "./Calculadora.module.css"
-import { useState } from "react"
-import Input from "../../Input/Input"
-import Button from "../../Button/Button"
+import style from "./Calculadora.module.css";
+import { useState } from "react";
+import Input from "../../Input/Input";
+import Button from "../../Button/Button";
 
+export default function Calculadora() {
+  const [num1, setNum] = useState("");
+  const [num2, setNum2] = useState("");
+  const [resultado, setResultado] = useState("");
 
-export default  function Calculadora(){
-    const[num1, setNum] = useState("");
-    const[num2, setNum2] = useState("");
-    const[resultado, setResultado] = useState("");
-
-    function calcular(op){
-        const n1 = parseFloat(num1);
-        const n2 = parseFloat(num2);
-        if(isNaN(n1) || isNaN(n2)){
-            setResultado("Digite Valores validos!!!");
-            return;
-        }
-        switch(op){
-            case "+":
-                setResultado(n1 + n2);
-                break
-            case "-":
-                setResultado(n1 - n2);
-                break
-            case "*":
-                setResultado(n1 * n2);
-                break;
-            case "/":
-                setResultado(n2===0 ? "Err: Divisao por Zero" : n1/n2);
-                break;
-            default                             
-        }
+  function calcular(op) {
+    const n1 = parseFloat(num1);
+    const n2 = parseFloat(num2);
+    if (isNaN(n1) || isNaN(n2)) {
+      setResultado("Digite Valores validos!!!");
+      return;
     }
-    return(
-        <>
-            <h1>Page calculadora</h1>
-            <div>
-                <Input
-                label= "Numero 01"
-                value={num1}
-                onChange={(e)=> setNum(e.target.value)}
-                placeholder= "Numero 01"
-                />
+    switch (op) {
+      case "+":
+        setResultado(n1 + n2);
+        break;
+      case "-":
+        setResultado(n1 - n2);
+        break;
+      case "*":
+        setResultado(n1 * n2);
+        break;
+      case "/":
+        setResultado(n2 === 0 ? "Err: Divisao por Zero" : n1 / n2);
+        break;
+      default:
+    }
+  }
+  return (
+    <>
+      <h1>Page calculadora</h1>
+      <div>
+        <Input
+          label="Numero 01"
+          value={num1}
+          onChange={(e) => setNum(e.target.value)}
+          placeholder="Numero 01"
+        />
 
-                <Input
-                label= "Numero 02"
-                value={num2}
-                onChange={(e)=> setNum2(e.target.value)}
-                placeholder= "Numero 02"
-                />
-                <div>
-                    <Button label="+" onClick={somar} type="success" />
-                    <Button label="-" onClick={subtrair} type="warning" />
-                    <Button label="×" onClick={multiplicar} type="info" />
-                    <Button label="÷" onClick={dividir} type="danger" />
-                </div>
-                <p>Resultado: {resultado}</p>
-            </div>
-
-
-        </>
-    )
+        <Input
+          label="Numero 02"
+          value={num2}
+          onChange={(e) => setNum2(e.target.value)}
+          placeholder="Numero 02"
+        />
+        <div>
+          <Button label="+" onClick={somar} type="success" />
+          <Button label="-" onClick={subtrair} type="warning" />
+          <Button label="×" onClick={multiplicar} type="info" />
+          <Button label="÷" onClick={dividir} type="danger" />
+        </div>
+        <p>Resultado: {resultado}</p>
+      </div>
+    </>
+  );
 }
