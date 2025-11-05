bff717ecb695ea4a7146240958dc5469fd41bb27
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2025-11-05 20:39:20 -0300

    a

--- Aula36/src/Pages/Home/Home.jsx
+++ Aula36/src/Pages/Home/Home.jsx
@@ -21,7 +21,7 @@ export default function Home() {
         setErro("");
       } catch (erro) {
         setErro(erro.message);
-        setPokemon(null);
+        setPokemon(null);  
       }
     }
 
